42785f9cb8b3e04500f8c4a7468e9f4cf87ae4d0
EnjoyKojima <majiko2627kojima@gmail.com>
2025-02-11 15:02:17 +0900

    change preset

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -4,7 +4,7 @@ export default defineNuxtConfig({
   devtools: { enabled: true },
 
   nitro: {
-    preset: "cloudflare_module",
+    preset: "cloudflare_durable",
     experimental: {
       tasks: true,
     },
